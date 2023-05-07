5e7dbf16017df795a28fa0b8cb484a8267051de7
bkuehnis <62989491+bkuehnis@users.noreply.github.com>
2023-05-07 22:30:33 +0200

    add passwords

--- freelancer4u-frontend/src/auth.config.js
+++ freelancer4u-frontend/src/auth.config.js
@@ -1,6 +1,6 @@
 const config = {
-    domain: "",
-    clientId: ""
+    domain: "dev-7cfpgihj661pm4v3.us.auth0.com",
+    clientId: "nOPfMbLk5VHO2WZPj0AUxMpjT02K2jmB"
 };
   
 export default config;
